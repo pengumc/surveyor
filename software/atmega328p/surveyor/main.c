e5466df253d93c783c22c2623e59dd8fab97cdb7
pengumc <michiel.van.der.coelen@gmail.com>
2020-05-02 19:34:24 +0200

    Add atmega328 track control

--- software/atmega328p/surveyor/main.c
+++ software/atmega328p/surveyor/main.c
@@ -12,12 +12,15 @@
 
 void smbus_slave_block_write_done()
 {
-	PORTD ^= (1 << PIND3);
+	
 }
 
 void smbus_slave_command_hook(uint8_t cmd)
 {
-	PORTD ^= (1 << PIND2);
+	// hex commands: 
+	// track A forward/backward: 0x04 0x08
+	// track B forward/backward: 0x10 0x20
+	PORTD = cmd & ((1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND5));
 }
 
 int main(void)
@@ -25,14 +28,9 @@ int main(void)
 	uint8_t comms_mem[10];
 	smbus_slave_init(0x48, 56, 0b00, comms_mem, comms_mem); // baudrate 62.5 kHz
 	
-	TCCR0A = (1 << WGM01); // CTC mode
-	TCCR0B = (1 << CS02) | (1 << CS00); // clk / 1024 (8e6 / 1024 = 7812.5 Hz)
-	TIMSK0 = (1 << OCIE0A); // overflow interrupt
-	OCR0A = 200; // 40 Hz
 	// D2 and D3 as output
-	DDRD = (1 << PIND2) | (1 << PIND3);
+	DDRD = (1 << PIND2) | (1 << PIND3) | (1 << PIND4) | (1 << PIND5);
 	sei();
-    /* Replace with your application code */
     while (1) 
     {
     }
